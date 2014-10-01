2c9ee9119ab213b5c231d7c711bae52f1d389675
Caqtus <amirantoronjadze@gmail.com>
2014-10-02 01:39:27 +0400

    SharedPrefs save&load added. not implemented yet

--- app/src/main/java/com/caqtus/tobuylist/MyActivity.java
+++ app/src/main/java/com/caqtus/tobuylist/MyActivity.java
@@ -2,6 +2,8 @@ package com.caqtus.tobuylist;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,21 +22,46 @@ public class MyActivity extends Activity {
 
         ListView listView = (ListView) findViewById(R.id.list);
 
+        //list values array
         String[] values = new String[]{
-          "Food", "Consumable", "tech", "Steam sales :3"
+
         };
 
+        //converting into array list
         final ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < values.length; i++){
             list.add(values[i]);
             System.out.println(list.get(i));
         }
 
+        //setting arraylist data to listView
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
 
     }
 
+
+    //saving in sharedPrefs
+        public boolean saveArray(String[] array, String arrayName, Context mContext){
+            SharedPreferences prefs = mContext.getSharedPreferences("preferencename", 0);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt(arrayName + "_size", array.length);
+                for (int i = 0; i < array.length; i++){
+                    editor.putString(arrayName + "_" + i, array[i]);
+                }
+            return editor.commit();
+        }
+
+    //reading from sharedPrefs
+    public String[] loadArray(String arrayName, Context mContext){
+        SharedPreferences prefs = mContext.getSharedPreferences("preferencename", 0);
+        int size = prefs.getInt(arrayName + "_size", 0);
+        String array[] = new String[size];
+        for (int i = 0; i<size; i++){
+            array[i]= prefs.getString(arrayName + "_" + i, null);
+        }
+        return array;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
