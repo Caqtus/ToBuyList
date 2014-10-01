dc02125fee66810fee1ec8c2f09506008ba0fe83
Amiran Toronjadze <amirantoronjadze@gmail.com>
2014-10-01 18:16:01 +0400

    Add project -list with working string array in it

---
+++ app/src/main/java/com/caqtus/tobuylist/MyActivity.java
@@ -0,0 +1,57 @@
+package com.caqtus.tobuylist;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        String[] values = new String[]{
+          "Food", "Consumable", "tech", "Steam sales"
+        };
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; i++){
+            list.add(values[i]);
+            System.out.println(list.get(i));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
